12d2b1bb4ea6490459b04d19781accec5062349f
liyang <liyang@leocool.com>
2017-02-27 18:24:19 +0800

    add untwist.h

---
+++ source/Untwist.h
@@ -0,0 +1,13 @@
+/***********************
+Do the Untwist (1006) from ZOJ
+By yangli.network@gmail.com @20170227
+************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}
