e361bfd1a1a6dd0d3162791cfff96607b78a4433
liyang <yangli.network@gmail.com>
2017-03-04 18:27:28 +0800

    complete the enigma problem

--- source/Enigma/Enigma.cpp
+++ source/Enigma/Enigma.cpp
@@ -8,7 +8,7 @@ By yangli.network@gmail.com @20170227
 using namespace std;
 
 const int MAX_LETER = 26;
-const int MAX_CHARACTER = 1000;
+const int MAX_CHARACTER = 5000; // Attantion !! string is better
 
 int leterCount;
 int rotorIndexs[3];
@@ -18,65 +18,65 @@ int cryptoCount;
 char cryptograph[MAX_CHARACTER];
 
 void decrypt() {
-	int index = 0;
+  int index = 0;
 
-	while (index < MAX_CHARACTER && cryptograph[index] != 0) {
-		char character = cryptograph[index++];
+  while (index < MAX_CHARACTER && cryptograph[index] != 0) {
+    char character = cryptograph[index++];
+    for (int i = 2; i >= 0; i--) {
+      char * rotor = rotors[i];
+      int rotorIndex = rotorIndexs[i];
 
-		for (int i = 2; i >= 0; i--) {
-			char * rotor = rotors[i];
-			int rotorIndex = rotorIndexs[i];
+      for (int j = 0; j < leterCount; j++) {
+	char plain = j + 'A';
 
-			for (int j = 0; j < leterCount; j++) {
-				char plain = j + 'A';
+	int cryptIndex = (j - rotorIndex + leterCount) % leterCount;
+	char crypt = (rotor[cryptIndex] - ('A' + cryptIndex) +
+		      j + leterCount) % leterCount + 'A';
 
-				int cryptIndex = (j - rotorIndex + leterCount) % leterCount;
-				char crypt = (rotor[cryptIndex] - ('A' + cryptIndex) + j + leterCount) % leterCount + 'A';
-
-				if (crypt == character) {
-					character = plain;
-					break;
-				}
-			}
-		}
-
-		rotorIndexs[0]++;
-		for (int i = 0; i < 3; i++) {
-			if (rotorIndexs[i] == leterCount) {
-				rotorIndexs[i] = 0;
-				if (i < 2) {
-					rotorIndexs[i + 1]++;
-				}
-			}
-		}
-
-		character = character - 'A' + 'a';
-		cout << character;
+	if (crypt == character) {
+	  character = plain;
+	  break;
 	}
-
-	cout << endl;
+      }
+    }
+    
+    rotorIndexs[0]++;
+    for (int i = 0; i < 3; i++) {
+      if (rotorIndexs[i] == leterCount) {
+	rotorIndexs[i] = 0;
+	if (i < 2) {
+	  rotorIndexs[i + 1]++;
+	}
+      }
+    }
+    
+    character = character - 'A' + 'a';
+    cout << character;
+  }
+  
+  cout << endl;
 }
 
 int main() {
-	int index = 0;
-	
-	while (cin >> leterCount && leterCount) {
-		cin >> rotors[0] >> rotors[1] >> rotors[2];
+  int index = 0;
+  
+  while (cin >> leterCount && leterCount) {
+    cin >> rotors[0] >> rotors[1] >> rotors[2];
+    
+    // decrypto;
+    if (++index > 1) {
+      cout << endl;
+    }
+    cout << "Enigma " << index << ":" << endl;
+    
+    cin >> cryptoCount;
+    for (int i = 0; i < cryptoCount; i++) {
+      cin >> cryptograph;
+      
+      rotorIndexs[0] = rotorIndexs[1] = rotorIndexs[2] = 0;
+      decrypt();
+    }
+  }
 
-		// decrypto;
-		if (++index > 1) {
-			cout << endl;
-		}
-		cout << "Enigma " << index << ":" << endl;
-
-		cin >> cryptoCount;
-		for (int i = 0; i < cryptoCount; i++) {
-			cin >> cryptograph;
-
-			rotorIndexs[0] = rotorIndexs[1] = rotorIndexs[2] = 0;
-			decrypt();
-		}
-	}
-
-	return 0;
+  return 0;
 }
