81267cca66a0041a5df627c1b734a20994390421
liyang <liyang@leocool.com>
2017-03-07 14:25:37 +0800

    update

--- source/Area/Area.cpp
+++ source/Area/Area.cpp
@@ -11,7 +11,7 @@ float min(float x, float y) {
 }
 
 float abs(float x) {
-  return x > 0 ? x : -x;
+  return x >= 0 ? x : -x;
 }
 
 struct Point {
@@ -36,32 +36,17 @@ bool isStraddle(Point &a1, Point &a2, Point &b1, Point &b2) {
 
 bool isIntersect(Point &a1, Point &a2, Point &b1, Point &b2) {
   float x1 = (b1.x - a1.x) * (a2.y - a1.y) - (b1.y - a1.y) * (a2.x - a1.x);
-  if (x1 == 0) {
-    return false;
-  }
-
   float x2 = (b2.x - a1.x) * (a2.y - a1.y) - (b2.y - a1.y) * (a2.x - a1.x);
-  if (x2 == 0 || x1 * x2 > 0) {
-    return false;
-  }
-
   float x3 = (a1.x - b1.x) * (b2.y - b1.y) - (a1.y - b1.y) * (b2.x - b1.x);
-  if (x3 == 0) {
-    return false;
-  }
-
   float x4 = (a2.x - b1.x) * (b2.y - b1.y) - (a2.y - b1.y) * (b2.x - b1.x);
-  if (x4 == 0 || x3 * x4 > 0) {
-    return false;
-  }
   
-  return true;
+  return x1 * x2 <= 0 && x3 * x4 <= 0;
 }
   
 int main() {
   int count;
   Point list[1000];
-  int index;
+  int index = 0;
   
   while(cin >> count && count) {
     // input
@@ -70,28 +55,22 @@ int main() {
     }
 
     bool isPolygon = count >= 3;
-      for (int i = 0; i < count && isPolygon; i++) {
-	Point a1 = list[(i + 1) % count];
-	Point a2 = list[i];
-	
-	for (int j = 0; j < i && isPolygon; j ++) {
-	  Point b1 = list[j + 1];
-	  Point b2 = list[j];
+	for (int i = 0; i < count && isPolygon; i++) {
+		Point a1 = list[(i + 1) % count];
+		Point a2 = list[i];
+		
+		for (int j = 0; j < i - 1 && isPolygon; j ++) {
+		  Point b1 = list[j + 1];
+		  Point b2 = list[j];
 
-	  if (i == (j + 1) || (i + 1 - j) == count) {
-	    if ((a1.x - a2.x) * (b1.y - b2.y) - (a1.y - a2.y) * (b1.x - b2.x) == 0) {
-	      isPolygon = false;
-	      break;
-	    }
-	  }
-	  else {
-	    if (isStraddle(a1, a2, b1, b2) && isIntersect(a1, a2, b1, b2)) {
-	      isPolygon = false;
-	      break;
-	    }
-	  }
-	}
-      }
+		  if ((i + 1 - j) != count) {
+		    if (isStraddle(a1, a2, b1, b2) && isIntersect(a1, a2, b1, b2)) {
+		      isPolygon = false;
+		      break;
+		    }
+		  }
+		}
+    }
 
     // output
     if (++index > 1) {
@@ -104,8 +83,8 @@ int main() {
       int j = 0;
       
       for (int i = 0; i < count; i++) {
-	j = (i + 1) % count;
-	area += list[i].x * list[j].y - list[i].y * list[j].x;
+		j = (i + 1) % count;
+		area += list[i].x * list[j].y - list[i].y * list[j].x;
       }
 
       printf("%.2lf\n", abs(area * 0.5));
