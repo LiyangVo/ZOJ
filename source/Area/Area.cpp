55c30789914d9a71bc1674e0000b449d1c21a08e
liyang <liyang@leocool.com>
2017-03-07 15:25:25 +0800

    using namespace std;
    float max(float x, float y) {
      return x > y ? x : y;
    }
    
    float min(float x, float y) {
      return x > y ? y : x;
    }
    
    float abs(float x) {
      return x >= 0 ? x : -x;
    }
    
    struct Point {
      float x;
      float y;
    };
    
    float cross(Point &b, Point &a1, Point &a2) {
      return (b.x - a1.x) * (a2.y - a1.y) - (b.y - a1.y) * (a2.x - a1.x);
    }
    
    bool isIntersect(Point &a1, Point &a2, Point &b1, Point &b2) {
      // rect
      if (min(b1.x, b2.x) > max(a1.x, a2.x) || max(b1.x, b2.x) < min(a1.x, a2.x) ||
          min(b1.y, b2.y) > max(a1.y, a2.y) || max(b1.y, b2.y) < min(a1.y, a2.y)) {
        return false;
      }
    
      // kuali
      return cross(b1, a1, a2) * cross(b2, a1, a2) <= 0 && cross(a1, b1, b2) * cross(a2, b1, b2) <= 0;
    }
    
    int main() {
      int count;
      Point list[1005];
      int index = 0;
    
      while(cin >> count && count) {
        // input
        for (int i = 0; i < count; i++) {
          cin >> list[i].x >> list[i].y;
        }
    
        bool isPolygon = count >= 3;
    	for (int i = 0; i < count && isPolygon; i++) {
    		for (int j = 0; j < i - 1 && isPolygon; j++) {
    		  if ((i + 1 - j) != count && isIntersect(list[(i + 1) % count], list[i], list[j + 1], list[j])) {
    		      isPolygon = false;
    		      break;
    		  }
    		}
        }
    
        // output
        if (++index > 1) {
          cout << endl;
        }
    
        cout << "Figure " << index << ": ";
        if (isPolygon) {
          float area = 0;
          for (int i = 0; i < count; i++) {
    		int j = (i + 1) % count;
    		area += list[i].x * list[j].y - list[i].y * list[j].x;
          }
    
          printf("%.2f\n", abs(area * 0.5));
        }
        else {
          cout << "Impossible" << endl;
        }
      }
    
      return 0;
    }

--- source/Area/Area.cpp
+++ source/Area/Area.cpp
@@ -19,28 +19,19 @@ struct Point {
   float y;
 };
 
-bool isStraddle(Point &a1, Point &a2, Point &b1, Point &b2) {
-  float minAx = min(a1.x, a2.x);
-  float maxAx = max(a1.x, a2.x);
-  float minBx = min(b1.x, b2.x);
-  float maxBx = max(b1.x, b2.x);
-
-  float minAy = min(a1.y, a2.y);
-  float maxAy = max(a1.y, a2.y);
-  float minBy = min(b1.y, b2.y);
-  float maxBy = max(b1.y, b2.y);
-
-  return ((minBx >= minAx && minBx <= maxAx) || (maxBx >= minAx && maxBx <= maxAx)) &&
-    ((minBy >= minAy && minBy <= maxAy) || (maxBy >= minAy && maxBy <= maxAy));
+float cross(Point &b, Point &a1, Point &a2) {
+  return (b.x - a1.x) * (a2.y - a1.y) - (b.y - a1.y) * (a2.x - a1.x);
 }
 
 bool isIntersect(Point &a1, Point &a2, Point &b1, Point &b2) {
-  float x1 = (b1.x - a1.x) * (a2.y - a1.y) - (b1.y - a1.y) * (a2.x - a1.x);
-  float x2 = (b2.x - a1.x) * (a2.y - a1.y) - (b2.y - a1.y) * (a2.x - a1.x);
-  float x3 = (a1.x - b1.x) * (b2.y - b1.y) - (a1.y - b1.y) * (b2.x - b1.x);
-  float x4 = (a2.x - b1.x) * (b2.y - b1.y) - (a2.y - b1.y) * (b2.x - b1.x);
+  // rect
+  if (min(b1.x, b2.x) > max(a1.x, a2.x) || max(b1.x, b2.x) < min(a1.x, a2.x) ||
+      min(b1.y, b2.y) > max(a1.y, a2.y) || max(b1.y, b2.y) < min(a1.y, a2.y)) {
+    return false;
+  }
   
-  return x1 * x2 <= 0 && x3 * x4 <= 0;
+  // kuali
+  return cross(b1, a1, a2) * cross(b2, a1, a2) <= 0 && cross(a1, b1, b2) * cross(a2, b1, b2) <= 0;
 }
   
 int main() {
@@ -56,18 +47,10 @@ int main() {
 
     bool isPolygon = count >= 3;
 	for (int i = 0; i < count && isPolygon; i++) {
-		Point a1 = list[(i + 1) % count];
-		Point a2 = list[i];
-		
-		for (int j = 0; j < i - 1 && isPolygon; j ++) {
-		  Point b1 = list[j + 1];
-		  Point b2 = list[j];
-
-		  if ((i + 1 - j) != count) {
-		    if (isStraddle(a1, a2, b1, b2) && isIntersect(a1, a2, b1, b2)) {
+		for (int j = 0; j < i - 1 && isPolygon; j++) {
+		  if ((i + 1 - j) != count && isIntersect(list[(i + 1) % count], list[i], list[j + 1], list[j])) {
 		      isPolygon = false;
 		      break;
-		    }
 		  }
 		}
     }
@@ -80,20 +63,17 @@ int main() {
     cout << "Figure " << index << ": ";
     if (isPolygon) {
       float area = 0;
-      int j = 0;
-      
       for (int i = 0; i < count; i++) {
-		j = (i + 1) % count;
+		int j = (i + 1) % count;
 		area += list[i].x * list[j].y - list[i].y * list[j].x;
       }
 
-      printf("%.2lf\n", abs(area * 0.5));
+      printf("%.2f\n", abs(area * 0.5));
     }
     else {
       cout << "Impossible" << endl;
     }
   }
-
 
   return 0;
 }
